c92600d9848cb54d6b80973c6543646e77814fac
prashantmm <pmmahindrakar03@gmail.com>
2022-12-21 21:56:10 +0530

    array pgm

---
+++ array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+counter=0
+Fruits[((counter++))]="P"
+Fruits[((counter++))]="R"
+Fruits[((counter++))]="A"
+Fruits[((counter++))]="S"
+Fruits[((counter++))]="H"
+Fruits[((counter++))]="A"
+Fruits[((counter++))]="N"
+Fruits[((counter++))]="T"
+echo ${Fruits[@]}
